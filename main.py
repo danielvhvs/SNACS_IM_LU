9d6c4a919e35584ced85f6ff037e43e5e629cfd8
Christian Steennis <csteennis@hotmail.nl>
2023-11-10 19:06:27 +0100

    experiment setup

--- main.py
+++ main.py
@@ -1,15 +1,43 @@
-import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 import ICmodel as icm
 import discountIC as dic
 import greedyIC as gic
 
-def main():
-    G = nx.gnm_random_graph(15000, 60000)
-    print(list(nx.descendants(G,1)))
-    return
+def run_model_with(G, k_max, p, mc, function):
+    spread = []
+    for k in tqdm(range(k_max)):
+        S = function(k)
+        spread.append(icm.IC(G, S, p, mc)[0])
 
+    return spread
+
+def main():
+    G = nx.read_edgelist('data/Wiki-Vote.txt.gz', create_using=nx.DiGraph)
+
+    k_max = 50
+    p = 0.01
+    mc = 20000
+
+    algorithms = {'DegreeDiscountIC': lambda k: dic.DDIC(G, k, p),
+                  'SingleDiscount': lambda k: dic.SD(G, k),
+                  'Random': lambda k: dic.random_sd(G, k),
+                #   greedy and new greedy are slow uncomment if you have some hours to spare
+                #   'NewGreedy': lambda k: gic.NewGreedy(G, k, p),
+                #   'Greedy': lambda k: gic.GeneralGreedy(G, k, p),
+                }
+
+    for name, algo in algorithms.items():
+        data = run_model_with(G, k_max, p, mc, algo)
+        plt.plot(data, label=name)
+
+    plt.legend()
+    plt.xlabel('seeds')
+    plt.ylabel('influence spread')
+    plt.show()
+
+    return
 
 if __name__ == "__main__":
     main()
